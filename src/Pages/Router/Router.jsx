9d69d04c18320b014dcb6510411bf874528f760e
Dusuna <dusunax@gmail.com>
2022-07-29 21:25:49 +0900

    note page 수정

--- src/Pages/Router/Router.jsx
+++ src/Pages/Router/Router.jsx
@@ -1,6 +1,6 @@
 import { Switch, Route } from "react-router-dom"
 import NotFound from '../NotFound/NotFound'
-import Login from '../../components/Login/Login'
+import Login from '../Login/Login'
 import Register from '../../components/Register/Register'
 import NoteList from '../../components/NoteList/NoteList'
 import Detail from '../Detail/Detail'
